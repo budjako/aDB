ec59034cbdd26de15b990d279b4e93c7a348c6e5
Adrian Paul <aplanacay@gmail.com>
2017-10-01 11:00:52 +0800

    Followed Dja's standards

--- insert/insert.py
+++ insert/insert.py
@@ -5,11 +5,11 @@ if sys.version_info[0] >= 3:
     raw_input = input
 
 keywords = ( 
-    'INSERT', 'INTO', 'VALUES',
+    'INSERT', 'INTO', 'VALUES', 'SET',
 )
 
 tokens = keywords + (
-    'COLUMN_NAME', 'TABLE_NAME', 'LPAREN', 'RPAREN', 'NAME', 'EQUALS' #'VALUE_LIST', 'ASSIGNMENT_LIST', 'COLUMN', 'VALUE_LIST',
+    'COLUMN_NAME', 'TABLE_NAME', 'LPAREN', 'RPAREN', 'EQUALS', 'STRING_LIT', 'INT_LIT', 'DOUBLE_LIT', 'DATE_LIT', 'COMMA', #'VALUE_LIST', 'ASSIGNMENT_LIST', 'COLUMN', 'VALUE_LIST',
     #'VALUE', 'ASSIGNMENT', 
 )
 
@@ -17,39 +17,40 @@ tables = ('student')
 
 t_COLUMN_NAME = r'(studno|studentname|birthday|degree|major|unitsearned|description|action|datefiled|dateresolved|cno|ctitle|cdesc|noofunits|haslab|semoffered|semester|acadyear|cno|section|time|maxstud)'
 t_TABLE_NAME = r'(student|studenthistory|course|courseoffering|studcourse)'
+t_STRING_LIT = r'\'.*\'|\".*\"'
 
-t_INSERT = r'INSERT'
 
-t_INTO = r'INTO'
-
-t_VALUES = r'VALUES'
+t_INSERT = r'insert'
+t_INTO = r'into'
+t_VALUES = r'values'
+t_SET = r'set'
 
 t_LPAREN = r'\('
-
 t_RPAREN = r'\)'
-
 t_EQUALS = r'='
-
-# t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
-
-def t_NAME(t):
-    r'[a-zA-Z][a-zA-Z0-9_]*'
-    # print(keywords)
-    # print("value: " + t.value.upper())
-    # print("type: " + t.type)
-    if t.value.upper() in keywords:
-        t.type = t.value.upper()
-        # print("pumasok " + t.value)
-        # print("newtype "+ t.type)
-    return t
-
-# t_TBLNAME = r'[A-Z]+'#r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_COMMA = r','
 
 t_ignore = " \t"
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
+
+def t_DATE_LIT(t):
+    r'\d{4}\-(0?[1-9]|10|11|12)\-(0?[1-9]|1[0-9]|2[0-9]|30|31)' # Year-Month-Day
+    t.value = '2017-01-01'
+    return t 
+    # t_value = date(2017, 10, 1)
+
+def t_DOUBLE_LIT(t):
+    r'\-?\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+def t_INT_LIT(t):
+    r'\-?\d+'
+    t.value = int(t.value)
+    return t
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
@@ -60,61 +61,26 @@ lex.lex()
 
 names = {}
 
-# tables = {
-#           'student' : {'StudNo', 'StudentName', 'Birthday', 'Degree', 'Major', 'UnitsEarned'}, 
-#           'studenthistory' : {'StudNo', 'Description', 'Action', 'DataFiled', 'DateResolved'}, 
-#           'course' : {'CNo', 'CTitle', 'CDesc', 'NoOfUnits', 'HasLab', 'SemOffered'}, 
-#           'courseoffering' : {'Semester', 'AcadYear', 'CNo', 'Section', 'Time', 'MaxStud'}, 
-#           'studcourse' : {'StudNo', 'CNo', 'Semester', 'AcadYear' } 
-#           }
-
-# columns = {'name'}
-
-# def p_statement_insert(p):
-#     "statement : INSERT"
-#     print(p[1])
-
-# def p_statement_into(p):
-#     'statement : INTO'
-#     print(p[1])
 def p_statement_query(p):
-    'statement : INSERT INTO TABLE_NAME'
-
-def p_insert_option(p):
-    """
-    insertoption : valuelist
-                |   assignmentlist
-    """
+    '''statement : INSERT INTO TABLE_NAME VALUES LPAREN valuelist RPAREN
+            | INSERT INTO TABLE_NAME SET assignmentlist'''
 
 def p_value_list(p):
-    'valuelist : VALUES LPAREN NAME RPAREN'
+    '''valuelist : expr
+            | expr COMMA valuelist'''
 
 def p_assignment_list(p):
-    'assignmentlist : NAME'
+    '''assignmentlist : assignment
+            | assignment COMMA assignment'''
 
+def p_assignment(p):
+    '''assignment : COLUMN_NAME EQUALS expr'''
 
-# def p_statement_tblname(p):
-
-# def p_expression_column(p):
-#         'column : NAME'
-#         if p[1] in columns:
-#             print(p[1])
-
-# def p_expression_tblname(p):
-#         'tblname : NAME'
-#         if p[1] in tables:
-#             print(p[1])
-#         else:
-#             print("Undefined table '%s'" % p[1])
-        # try:
-        #     p[0] = tables[p[1]]
-        # except LookupError:
-        #     print("Undefined table '%s'" % p[1])
-        #     p[0] = 0
-
-# def p_statement_tblname(p):
-#     'statement : NAME'
-#     print("high" + p[1])
+def p_expr(p):
+    '''expr : STRING_LIT
+            | INT_LIT
+            | DOUBLE_LIT
+            | DATE_LIT'''
 
 def p_error(p):
     if p:
@@ -128,6 +94,11 @@ yacc.yacc()
 while 1:
     try:
         s = raw_input('sql > ')
+        s = s.lower()
+        lex.input(s)
+        for tok in iter(lex.token, None):
+            print(tok)
+            print(repr(tok.type), repr(tok.value))
     except EOFError:
         break
     if not s:
