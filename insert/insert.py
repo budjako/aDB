1851111598a4d0f338e21ad2c1ffa17fc6861a16
Adrian Paul <aplanacay@gmail.com>
2017-10-01 09:36:52 +0800

    Added insert grammar

--- insert/insert.py
+++ insert/insert.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, "../..")
+sys.path.insert(0, "..")
 
 if sys.version_info[0] >= 3:
     raw_input = input
@@ -9,11 +9,14 @@ keywords = (
 )
 
 tokens = keywords + (
-    'SPACE', 'LPAREN', 'RPAREN', 'NAME', #'VALUE_LIST', 'ASSIGNMENT_LIST', 'COLUMN', 'VALUE_LIST',
+    'COLUMN_NAME', 'TABLE_NAME', 'LPAREN', 'RPAREN', 'NAME', 'EQUALS' #'VALUE_LIST', 'ASSIGNMENT_LIST', 'COLUMN', 'VALUE_LIST',
     #'VALUE', 'ASSIGNMENT', 
 )
 
 tables = ('student')
+
+t_COLUMN_NAME = r'(studno|studentname|birthday|degree|major|unitsearned|description|action|datefiled|dateresolved|cno|ctitle|cdesc|noofunits|haslab|semoffered|semester|acadyear|cno|section|time|maxstud)'
+t_TABLE_NAME = r'(student|studenthistory|course|courseoffering|studcourse)'
 
 t_INSERT = r'INSERT'
 
@@ -21,11 +24,11 @@ t_INTO = r'INTO'
 
 t_VALUES = r'VALUES'
 
-t_SPACE = r'[ ]+'
-
 t_LPAREN = r'\('
 
 t_RPAREN = r'\)'
+
+t_EQUALS = r'='
 
 # t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
@@ -42,7 +45,7 @@ def t_NAME(t):
 
 # t_TBLNAME = r'[A-Z]+'#r'[a-zA-Z_][a-zA-Z0-9_]*'
 
-# t_ignore = " \t"
+t_ignore = " \t"
 
 def t_newline(t):
     r'\n+'
@@ -57,15 +60,15 @@ lex.lex()
 
 names = {}
 
-tables = {
-          'student' : {'StudNo', 'StudentName', 'Birthday', 'Degree', 'Major', 'UnitsEarned'}, 
-          'studenthistory' : {'StudNo', 'Description', 'Action', 'DataFiled', 'DateResolved'}, 
-          'course' : {'CNo', 'CTitle', 'CDesc', 'NoOfUnits', 'HasLab', 'SemOffered'}, 
-          'courseoffering' : {'Semester', 'AcadYear', 'CNo', 'Section', 'Time', 'MaxStud'}, 
-          'studcourse' : {'StudNo', 'CNo', 'Semester', 'AcadYear' } 
-          }
+# tables = {
+#           'student' : {'StudNo', 'StudentName', 'Birthday', 'Degree', 'Major', 'UnitsEarned'}, 
+#           'studenthistory' : {'StudNo', 'Description', 'Action', 'DataFiled', 'DateResolved'}, 
+#           'course' : {'CNo', 'CTitle', 'CDesc', 'NoOfUnits', 'HasLab', 'SemOffered'}, 
+#           'courseoffering' : {'Semester', 'AcadYear', 'CNo', 'Section', 'Time', 'MaxStud'}, 
+#           'studcourse' : {'StudNo', 'CNo', 'Semester', 'AcadYear' } 
+#           }
 
-columns = {'name'}
+# columns = {'name'}
 
 # def p_statement_insert(p):
 #     "statement : INSERT"
@@ -75,8 +78,7 @@ columns = {'name'}
 #     'statement : INTO'
 #     print(p[1])
 def p_statement_query(p):
-    'statement : INSERT SPACE INTO SPACE tblname SPACE insertoption'
-    # "statement : tblname"
+    'statement : INSERT INTO TABLE_NAME'
 
 def p_insert_option(p):
     """
@@ -85,7 +87,7 @@ def p_insert_option(p):
     """
 
 def p_value_list(p):
-    'valuelist : VALUES SPACE LPAREN NAME RPAREN'
+    'valuelist : VALUES LPAREN NAME RPAREN'
 
 def p_assignment_list(p):
     'assignmentlist : NAME'
@@ -98,12 +100,12 @@ def p_assignment_list(p):
 #         if p[1] in columns:
 #             print(p[1])
 
-def p_expression_tblname(p):
-        'tblname : NAME'
-        if p[1] in tables:
-            print(p[1])
-        else:
-            print("Undefined table '%s'" % p[1])
+# def p_expression_tblname(p):
+#         'tblname : NAME'
+#         if p[1] in tables:
+#             print(p[1])
+#         else:
+#             print("Undefined table '%s'" % p[1])
         # try:
         #     p[0] = tables[p[1]]
         # except LookupError:
