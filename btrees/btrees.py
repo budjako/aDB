b87ed9cd8a64098098c6acf56ba60b5df1b78193
gerbenee <gerbene5@gmail.com>
2017-11-09 18:57:44 +0800

    Insert and select updates

--- btrees/btrees.py
+++ btrees/btrees.py
@@ -95,14 +95,50 @@ for i in btree.keys():
 
 operation = "insert"
 table_selected = "student"
+#table_selected = "studenthistory"
+#table_selected = "course"
+#table_selected = "courseoffering" #1. Acad Year as b-tree key 2. list of primary key as b-tree key
+#table_selected = "studcourse" #1. Auto-increment primary key
+
 value_list = "'2011-12314','Gerald Emalada','1995-06-10','BSCS','null','18'"
+#value_list = "'2011-12314','abcdesc','complete','string1','string2'"
+#value_list = "'CMSC 123','Data Structures','Data Structures desc','3','1','2nd'"
+#value_list = "'2nd','2017-2018','CMSC 123','AB','1-2','120'"
+
 val_ins = '';
 col_name = ' studno , studentname , birthday , degree , major , unitsearned ';
-value_list_bool = False;
-column_name_bool = False;
-assignment_list = " 'studno' = '2012-29339' , 'studentname' = 'Gerald Emalada' , 'birthday' = '1995-06-10' , 'degree' = 'bscs' , 'major' = 'null' , 'unitsearned' = '18' ";
+#col_name = ' studno , desription , action , datefiled , dateresolved '
+#col_name = ' cno , ctitle , cdesc , noofunits , haslab , semoffered '
+#col_name = ' semester , acadyear , cno , section , time , maxstud '
 
+value_list_bool = True;
+column_name_bool = False;
+
+assignment_list = " 'studno' = '2011-12314' , 'studentname' = 'Gerald Emalada' , 'birthday' = '1995-06-10' , 'degree' = 'bscs' , 'major' = 'null' , 'unitsearned' = '18' ";
+#assignment_list = " 'studno' = '2011-12314' , 'description' = 'desc1' , 'action' = 'completed' , 'datefiled' = 'filed1' , 'dateresolved' = 'resolved1' "
+#assignment_list = " 'cno' = 'CMSC 123' , 'ctitle' = 'Data Structures' , 'cdesc' = 'Data Structures desc' , 'noofunits' = '3' , 'haslab' = '1' , 'semoffered' = '2nd' "
+#assignment_list = " 'semester' = '2nd' , 'acadyear' = '2017-2018' , 'cno' = 'CMSC 123' , 'section' = 'AB' , 'time' = '1-2' , 'maxstud' = '120' "
+
+student_primary_key = "studno"
+studenthistory_primary_key = 'studno'
+course_primary_key = 'cno'
+courseoffering_primary_key = ['semester','acadyear','cno','section']
+curr_table_primary = ''
+
+print(cols)
+#cols = ['studno','description','action','datefiled','dateresolved'] #for studenthistory testing
+#cols = ['cno', 'ctitle', 'cdesc', 'noofunits', 'haslab', 'secmoffered'] #for course testing
+#cols = ['semester', 'acadyear', 'cno', 'section', 'time', 'maxstud'] #for courseoffering testing
+print(cols)
 if operation == 'insert':
+    if table_selected == 'student':     #For primary key of table
+        curr_table_primary = student_primary_key
+    elif table_selected == 'studenthistory':
+        curr_table_primary = studenthistory_primary_key
+    elif table_selected == 'course':
+        curr_table_primary = course_primary_key
+
+
     if table_selected == 'student':
         if value_list_bool and not column_name_bool: #first case input
             value_list = value_list.split(',')
@@ -110,8 +146,12 @@ if operation == 'insert':
                 value_list[i] = value_list[i].rstrip('\'')
                 value_list[i] = value_list[i].lstrip('\'')
             if value_list[0] not in btree:
-                btree.update({value_list[0]: {cols[1]: value_list[1], cols[2]: value_list[2], cols[3]: value_list[3], cols[4]: value_list[4], cols[5]: value_list[5]}})
-                print(value_list)
+                if len(value_list) == 6:
+                    print("value list length is 6")
+                    btree.update({value_list[0]: {cols[1]: value_list[1], cols[2]: value_list[2], cols[3]: value_list[3], cols[4]: value_list[4], cols[5]: value_list[5]}})
+                    print(value_list)
+                else:
+                    print("Column values are lacking")
             else:
                 print("Already in BTree")
 
@@ -122,15 +162,18 @@ if operation == 'insert':
                 value_list[i] = value_list[i].rstrip('\'')
                 value_list[i] = value_list[i].lstrip('\'')
             col_name = col_name.split(',')
-            key_index = col_name.index("studno")
-            key_col_name = col_name.pop(key_index)
-            key_value_list = value_list.pop(key_index)
-            dict_cols_vals = dict(zip(col_name,value_list))
-
-            if key_value_list not in btree:
-                btree.update({key_value_list: dict_cols_vals})
+            if "studno" not in col_name:
+                print("No primary key")
             else:
-                print("Already in BTree")
+                key_index = col_name.index("studno")
+                key_col_name = col_name.pop(key_index)
+                key_value_list = value_list.pop(key_index)
+                dict_cols_vals = dict(zip(col_name,value_list))
+
+                if key_value_list not in btree:
+                    btree.update({key_value_list: dict_cols_vals})
+                else:
+                    print("Already in BTree")
 
         elif not value_list_bool and not column_name_bool: #third case input
             assignment_list = assignment_list.split(',')
@@ -145,20 +188,207 @@ if operation == 'insert':
                 col_name.append(col_ext)
                 value_list.append(val_ext)
 
-            key_index = col_name.index("studno")
-            key_col_name = col_name.pop(key_index)
-            key_value_list = value_list.pop(key_index)
-            dict_cols_vals = dict(zip(col_name,value_list))
+            if "studno" not in col_name:
+                print("No primary key")
+            else:
+                key_index = col_name.index("studno")
+                key_col_name = col_name.pop(key_index)
+                key_value_list = value_list.pop(key_index)
+                dict_cols_vals = dict(zip(col_name,value_list))
 
-            if key_value_list not in btree:
-                btree.update({key_value_list: dict_cols_vals})
+                if key_value_list not in btree:
+                    btree.update({key_value_list: dict_cols_vals})
+                else:
+                    print("Already in BTree")
+
+    elif table_selected == 'studenthistory':
+        if value_list_bool and not column_name_bool: #first case input
+            value_list = value_list.split(',')
+            for i in range(0, len(value_list)):
+                value_list[i] = value_list[i].rstrip('\'')
+                value_list[i] = value_list[i].lstrip('\'')
+            if value_list[0] not in btree:
+                if len(value_list) == 5:
+                    print("value list length is 5")
+                    btree.update({value_list[0]: {cols[1]: value_list[1], cols[2]: value_list[2], cols[3]: value_list[3], cols[4]: value_list[4]}})
+                else:
+                    print("Column values are lacking")
             else:
                 print("Already in BTree")
 
-print(list(btree))
-print(btree["2012-29339"])
+        elif value_list_bool and column_name_bool: #second case input
+            col_name = col_name.replace(" ", "")
+            value_list = value_list.split(',')
+            for i in range(0, len(value_list)):
+                value_list[i] = value_list[i].rstrip('\'')
+                value_list[i] = value_list[i].lstrip('\'')
+            col_name = col_name.split(',')
+            if "studno" not in col_name:
+                print("No primary key")
+            else:
+                key_index = col_name.index("studno")
+                key_col_name = col_name.pop(key_index)
+                key_value_list = value_list.pop(key_index)
+                dict_cols_vals = dict(zip(col_name,value_list))
 
-select = '*'
+                if key_value_list not in btree:
+                    btree.update({key_value_list: dict_cols_vals})
+                else:
+                    print("Already in BTree")
+
+        elif not value_list_bool and not column_name_bool: #third case input
+            assignment_list = assignment_list.split(',')
+            insert_list = []
+            col_name = []
+            value_list = []
+
+            for i in range(0,len(assignment_list)):
+                split_list = assignment_list[i].split("=")
+                col_ext = split_list[0][2:-2]
+                val_ext = split_list[1][2:-2]
+                col_name.append(col_ext)
+                value_list.append(val_ext)
+
+            if "studno" not in col_name:
+                print("No primary key")
+            else:
+                key_index = col_name.index("studno")
+                key_col_name = col_name.pop(key_index)
+                key_value_list = value_list.pop(key_index)
+                dict_cols_vals = dict(zip(col_name,value_list))
+
+                if key_value_list not in btree:
+                    btree.update({key_value_list: dict_cols_vals})
+                else:
+                    print("Already in BTree")
+
+    elif table_selected == 'course':
+        if value_list_bool and not column_name_bool: #first case input
+            value_list = value_list.split(',')
+            for i in range(0, len(value_list)):
+                value_list[i] = value_list[i].rstrip('\'')
+                value_list[i] = value_list[i].lstrip('\'')
+            if value_list[0] not in btree:
+                if len(value_list) == 6:
+                    print("value list length is 6")
+                    btree.update({value_list[0]: {cols[1]: value_list[1], cols[2]: value_list[2], cols[3]: value_list[3], cols[4]: value_list[4], cols[5]: value_list[5]}})
+                else:
+                    print("Column values are lacking")
+            else:
+                print("Already in BTree")
+
+        elif value_list_bool and column_name_bool: #second case input
+            col_name = col_name.replace(" ", "")
+            value_list = value_list.split(',')
+            for i in range(0, len(value_list)):
+                value_list[i] = value_list[i].rstrip('\'')
+                value_list[i] = value_list[i].lstrip('\'')
+            col_name = col_name.split(',')
+            if "cno" not in col_name:
+                print("No primary key")
+            else:
+                key_index = col_name.index("cno")
+                key_col_name = col_name.pop(key_index)
+                key_value_list = value_list.pop(key_index)
+                dict_cols_vals = dict(zip(col_name,value_list))
+
+                if key_value_list not in btree:
+                    btree.update({key_value_list: dict_cols_vals})
+                else:
+                    print("Already in BTree")
+
+        elif not value_list_bool and not column_name_bool: #third case input
+            assignment_list = assignment_list.split(',')
+            insert_list = []
+            col_name = []
+            value_list = []
+
+            for i in range(0,len(assignment_list)):
+                split_list = assignment_list[i].split("=")
+                col_ext = split_list[0][2:-2]
+                val_ext = split_list[1][2:-2]
+                col_name.append(col_ext)
+                value_list.append(val_ext)
+
+            if "cno" not in col_name:
+                print("No primary key")
+            else:
+                key_index = col_name.index("cno")
+                key_col_name = col_name.pop(key_index)
+                key_value_list = value_list.pop(key_index)
+                dict_cols_vals = dict(zip(col_name,value_list))
+
+                if key_value_list not in btree:
+                    btree.update({key_value_list: dict_cols_vals})
+                else:
+                    print("Already in BTree")
+
+    elif table_selected == 'courseoffering':
+        if value_list_bool and not column_name_bool: #first case input
+            value_list = value_list.split(',')
+            for i in range(0, len(value_list)):
+                value_list[i] = value_list[i].rstrip('\'')
+                value_list[i] = value_list[i].lstrip('\'')
+            if value_list[0] not in btree:
+                if len(value_list) == 6:
+                    print("value list length is 6")
+                    btree.update({value_list[0]: {cols[1]: value_list[1], cols[2]: value_list[2], cols[3]: value_list[3], cols[4]: value_list[4], cols[5]: value_list[5]}})
+                else:
+                    print("Column values are lacking")
+            else:
+                print("Already in BTree")
+
+        elif value_list_bool and column_name_bool: #second case input
+            col_name = col_name.replace(" ", "")
+            value_list = value_list.split(',')
+            for i in range(0, len(value_list)):
+                value_list[i] = value_list[i].rstrip('\'')
+                value_list[i] = value_list[i].lstrip('\'')
+            col_name = col_name.split(',')
+            if "cno" not in col_name:
+                print("No primary key")
+            else:
+                key_index = col_name.index("cno")
+                key_col_name = col_name.pop(key_index)
+                key_value_list = value_list.pop(key_index)
+                dict_cols_vals = dict(zip(col_name,value_list))
+
+                if key_value_list not in btree:
+                    btree.update({key_value_list: dict_cols_vals})
+                else:
+                    print("Already in BTree")
+
+        elif not value_list_bool and not column_name_bool: #third case input
+            assignment_list = assignment_list.split(',')
+            insert_list = []
+            col_name = []
+            value_list = []
+
+            for i in range(0,len(assignment_list)):
+                split_list = assignment_list[i].split("=")
+                col_ext = split_list[0][2:-2]
+                val_ext = split_list[1][2:-2]
+                col_name.append(col_ext)
+                value_list.append(val_ext)
+
+            if "cno" not in col_name:
+                print("No primary key")
+            else:
+                key_index = col_name.index("cno")
+                key_col_name = col_name.pop(key_index)
+                key_value_list = value_list.pop(key_index)
+                dict_cols_vals = dict(zip(col_name,value_list))
+
+                if key_value_list not in btree:
+                    btree.update({key_value_list: dict_cols_vals})
+                else:
+                    print("Already in BTree")
+
+
+print(list(btree))
+#print(btree["2012-29339"])
+
+select = 'studentname,birthday,studno'
 
 for j, k in zip(btree.keys(), btree.values()):
     if select == '*':
@@ -170,4 +400,57 @@ for j, k in zip(btree.keys(), btree.values()):
         print(k['unitsearned'] + " ", end="")
         print("")
     else:
-        print(k[select])
+        if select == 'studno':
+            print(j)
+        else:
+            select_query = select.split(',')
+            for item in select_query:
+                if item == 'studno':
+                    print(j + " ", end="")
+                else:
+                    print(k[item] + " ", end="")
+            print("")
+'''
+#for studenthistory
+for j, k in zip(btree.keys(), btree.values()):
+    if select == '*':
+        print(j + " ", end="")
+        print(k['description'] + " ", end="")
+        print(k['action'] + " ", end="")
+        print(k['datefiled'] + " ", end="")
+        print(k['dateresolved'] + " ", end="")
+        print("")
+    else:
+        if select == 'studno':
+            print(j)
+        else:
+            select_query = select.split(',')
+            for item in select_query:
+                if item == 'studno':
+                    print(j + " ", end="")
+                else:
+                    print(k[item] + " ", end="")
+            print("")
+
+#for course
+for j, k in zip(btree.keys(), btree.values()):
+    if select == '*':
+        print(j + " ", end="")
+        print(k['ctitle'] + " ", end="")
+        print(k['cdesc'] + " ", end="")
+        print(k['noofunits'] + " ", end="")
+        print(k['haslab'] + " ", end="")
+        print(k['semoffered'] + " ", end="")
+        print("")
+    else:
+        if select == 'cno':
+            print(j)
+        else:
+            select_query = select.split(',')
+            for item in select_query:
+                if item == 'cno':
+                    print(j + " ", end="")
+                else:
+                    print(k[item] + " ", end="")
+            print("")
+'''
