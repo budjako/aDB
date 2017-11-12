5d552ce7aa783c716b785a6a21220e8ce0c17c30
Gerald Emalada <geraldemalada@gmail.com>
2017-11-13 00:00:46 +0800

    Inserts None on empty columns

--- btrees/btrees.py
+++ btrees/btrees.py
@@ -94,32 +94,36 @@ for i in btree.keys():
 
 
 operation = "insert"
-#table_selected = "student"
+#operation = "select"
+
+
+
+table_selected = "student"
 #table_selected = "studenthistory"
 #table_selected = "course"
-table_selected = "courseoffering" #1. Four btrees
+#table_selected = "courseoffering" #1. Four btrees
 #table_selected = "studcourse" #1. Auto-increment primary key
 
-#value_list = "'2011-12314','Gerald Emalada','1995-06-10','BSCS','null','18'"
+value_list = "'2011-12314','Gerald Emalada','1995-06-10','BSCS','NULL','18'"
 #value_list = "'2011-12314','abcdesc','complete','string1','string2'"
 #value_list = "'CMSC 123','Data Structures','Data Structures desc','3','1','2nd'"
-value_list = "'2nd','2017-2018','CMSC 123','AB','1-2','120'"
+#value_list = "'2nd','2017-2018','CMSC 123','AB','1-2','120'"
 #value_list = "'2011-12314','CMSC 123','2nd','2017-2018'"
 
 val_ins = '';
-#col_name = ' studno , studentname , birthday , degree , major , unitsearned ';
+col_name = ' studno , studentname , birthday , degree , major , unitsearned ';
 #col_name = ' studno , desription , action , datefiled , dateresolved '
 #col_name = ' cno , ctitle , cdesc , noofunits , haslab , semoffered '
-col_name = ' semester , acadyear , cno , section , time , maxstud '
+#col_name = ' semester , acadyear , cno , section , time , maxstud '
 #col_name = ' studno , cno , semester , acadyear '
 
-value_list_bool = False;
+value_list_bool = True;
 column_name_bool = False;
 
-#assignment_list = " 'studno' = '2011-12314' , 'studentname' = 'Gerald Emalada' , 'birthday' = '1995-06-10' , 'degree' = 'bscs' , 'major' = 'null' , 'unitsearned' = '18' ";
+assignment_list = " 'studno' = '2011-12314' , 'studentname' = 'Gerald Emalada' , 'birthday' = '1995-06-10' , 'degree' = 'bscs' , 'major' = 'null' , 'unitsearned' = '18' ";
 #assignment_list = " 'studno' = '2011-12314' , 'description' = 'desc1' , 'action' = 'completed' , 'datefiled' = 'filed1' , 'dateresolved' = 'resolved1' "
 #assignment_list = " 'cno' = 'CMSC 123' , 'ctitle' = 'Data Structures' , 'cdesc' = 'Data Structures desc' , 'noofunits' = '3' , 'haslab' = '1' , 'semoffered' = '2nd' "
-assignment_list = " 'semester' = '2nd' , 'acadyear' = '2017-2018' , 'cno' = 'CMSC 123' , 'section' = 'AB' , 'time' = '1-2' , 'maxstud' = '120' "
+#assignment_list = " 'semester' = '2nd' , 'acadyear' = '2017-2018' , 'cno' = 'CMSC 123' , 'section' = 'AB' , 'time' = '1-2' , 'maxstud' = '120' "
 #assignment_list = " 'studno' = '2011-12314' , 'cno' = 'CMSC 123' , 'semester' = '2nd' , 'acadyear' = '2nd' "
 
 student_primary_key = "studno"
@@ -131,11 +135,10 @@ courseoffering_primary_key = ['semester','acadyear','cno','section']
 print(cols)
 #cols = ['studno','description','action','datefiled','dateresolved'] #for studenthistory testing
 #cols = ['cno', 'ctitle', 'cdesc', 'noofunits', 'haslab', 'secmoffered'] #for course testing
-cols = ['semester', 'acadyear', 'cno', 'section', 'time', 'maxstud'] #for courseoffering testing
+#cols = ['semester', 'acadyear', 'cno', 'section', 'time', 'maxstud'] #for courseoffering testing
 #cols = ['studno' , 'cno', 'semester', 'acadyear']
 print(cols)
 if operation == 'insert':
-
     if table_selected == 'student':
         if value_list_bool and not column_name_bool: #first case input
             value_list = value_list.split(',')
@@ -146,7 +149,7 @@ if operation == 'insert':
                 if len(value_list) == 6:
                     print("value list length is 6")
                     btree.update({value_list[0]: {cols[1]: value_list[1], cols[2]: value_list[2], cols[3]: value_list[3], cols[4]: value_list[4], cols[5]: value_list[5]}})
-                    print(value_list)
+                    print(btree[value_list[0]])
                 else:
                     print("Column values are lacking")
             else:
@@ -166,6 +169,17 @@ if operation == 'insert':
                 key_col_name = col_name.pop(key_index)
                 key_value_list = value_list.pop(key_index)
                 dict_cols_vals = dict(zip(col_name,value_list))
+
+                if 'studentname' not in dict_cols_vals:
+                    dict_cols_vals['studentname'] = None;
+                if 'birthday' not in dict_cols_vals:
+                    dict_cols_vals['birthday'] = None;
+                if 'degree' not in dict_cols_vals:
+                    dict_cols_vals['degree'] = None;
+                if 'major' not in dict_cols_vals:
+                    dict_cols_vals['major'] = None;
+                if 'unitsearned' not in dict_cols_vals:
+                    dict_cols_vals['unitsearned'] = None;
 
                 if key_value_list not in btree:
                     btree.update({key_value_list: dict_cols_vals})
@@ -192,6 +206,18 @@ if operation == 'insert':
                 key_col_name = col_name.pop(key_index)
                 key_value_list = value_list.pop(key_index)
                 dict_cols_vals = dict(zip(col_name,value_list))
+
+                if 'studentname' not in dict_cols_vals:
+                    dict_cols_vals['studentname'] = None;
+                if 'birthday' not in dict_cols_vals:
+                    dict_cols_vals['birthday'] = None;
+                if 'degree' not in dict_cols_vals:
+                    dict_cols_vals['degree'] = None;
+                if 'major' not in dict_cols_vals:
+                    dict_cols_vals['major'] = None;
+                if 'unitsearned' not in dict_cols_vals:
+                    dict_cols_vals['unitsearned'] = None;
+
 
                 if key_value_list not in btree:
                     btree.update({key_value_list: dict_cols_vals})
@@ -228,6 +254,15 @@ if operation == 'insert':
                 key_value_list = value_list.pop(key_index)
                 dict_cols_vals = dict(zip(col_name,value_list))
 
+                if 'description' not in dict_cols_vals:
+                    dict_cols_vals['description'] = None;
+                if 'action' not in dict_cols_vals:
+                    dict_cols_vals['action'] = None;
+                if 'datefiled' not in dict_cols_vals:
+                    dict_cols_vals['datefiled'] = None;
+                if 'dateresolved' not in dict_cols_vals:
+                    dict_cols_vals['dateresolved'] = None;
+
                 if key_value_list not in btree:
                     btree.update({key_value_list: dict_cols_vals})
                 else:
@@ -253,6 +288,15 @@ if operation == 'insert':
                 key_col_name = col_name.pop(key_index)
                 key_value_list = value_list.pop(key_index)
                 dict_cols_vals = dict(zip(col_name,value_list))
+
+                if 'description' not in dict_cols_vals:
+                    dict_cols_vals['description'] = None;
+                if 'action' not in dict_cols_vals:
+                    dict_cols_vals['action'] = None;
+                if 'datefiled' not in dict_cols_vals:
+                    dict_cols_vals['datefiled'] = None;
+                if 'dateresolved' not in dict_cols_vals:
+                    dict_cols_vals['dateresolved'] = None;
 
                 if key_value_list not in btree:
                     btree.update({key_value_list: dict_cols_vals})
@@ -289,6 +333,17 @@ if operation == 'insert':
                 key_value_list = value_list.pop(key_index)
                 dict_cols_vals = dict(zip(col_name,value_list))
 
+                if 'ctitle' not in dict_cols_vals:
+                    dict_cols_vals['ctitle'] = None;
+                if 'cdesc' not in dict_cols_vals:
+                    dict_cols_vals['cdesc'] = None;
+                if 'noofunits' not in dict_cols_vals:
+                    dict_cols_vals['noofunits'] = None;
+                if 'haslab' not in dict_cols_vals:
+                    dict_cols_vals['haslab'] = None;
+                if 'semoffered' not in dict_cols_vals:
+                    dict_cols_vals['semoffered'] = None;
+
                 if key_value_list not in btree:
                     btree.update({key_value_list: dict_cols_vals})
                 else:
@@ -314,6 +369,17 @@ if operation == 'insert':
                 key_col_name = col_name.pop(key_index)
                 key_value_list = value_list.pop(key_index)
                 dict_cols_vals = dict(zip(col_name,value_list))
+
+                if 'ctitle' not in dict_cols_vals:
+                    dict_cols_vals['ctitle'] = None;
+                if 'cdesc' not in dict_cols_vals:
+                    dict_cols_vals['cdesc'] = None;
+                if 'noofunits' not in dict_cols_vals:
+                    dict_cols_vals['noofunits'] = None;
+                if 'haslab' not in dict_cols_vals:
+                    dict_cols_vals['haslab'] = None;
+                if 'semoffered' not in dict_cols_vals:
+                    dict_cols_vals['semoffered'] = None;
 
                 if key_value_list not in btree:
                     btree.update({key_value_list: dict_cols_vals})
@@ -370,6 +436,11 @@ if operation == 'insert':
 
                 dict_cols_vals = dict(zip(col_name,value_list))
 
+                if 'time' not in dict_cols_vals:
+                    dict_cols_vals['time'] = None;
+                if 'maxstud' not in dict_cols_vals:
+                    dict_cols_vals['maxstud'] = None;
+
                 primary_key_string = key_value_list[0] + "," + key_value_list[1] + "," + key_value_list[2] + "," + key_value_list[3]
                 if primary_key_string not in btree:
                     new_dict_container = {key_col_name[0]: key_value_list[0], key_col_name[1]: key_value_list[1], key_col_name[2]: key_value_list[2], key_col_name[3]: key_value_list[3]}
@@ -416,6 +487,11 @@ if operation == 'insert':
 
                 dict_cols_vals = dict(zip(col_name,value_list))
 
+                if 'time' not in dict_cols_vals:
+                    dict_cols_vals['time'] = None;
+                if 'maxstud' not in dict_cols_vals:
+                    dict_cols_vals['maxstud'] = None;
+
                 primary_key_string = key_value_list[0] + "," + key_value_list[1] + "," + key_value_list[2] + "," + key_value_list[3]
 
                 if primary_key_string not in btree:
@@ -435,7 +511,8 @@ if operation == 'insert':
                     print("value list length is 4")
                     btreelen = str(len(btree) + 1)
                     btree.update({btreelen + "": {cols[0]: value_list[0], cols[1]: value_list[1], cols[2]: value_list[2], cols[3]: value_list[3]}})
-                    print(btree['5'])
+                else:
+                    print("Column values are lacking")
 
         elif value_list_bool and column_name_bool: #second case input
             col_name = col_name.replace(" ", "")
@@ -446,8 +523,17 @@ if operation == 'insert':
             col_name = col_name.split(',')
             dict_cols_vals = dict(zip(col_name,value_list))
 
+            if 'studno' not in dict_cols_vals:
+                    dict_cols_vals['studno'] = None;
+            if 'cno' not in dict_cols_vals:
+                    dict_cols_vals['cno'] = None;
+            if 'semester' not in dict_cols_vals:
+                    dict_cols_vals['semester'] = None;
+            if 'acadyear' not in dict_cols_vals:
+                    dict_cols_vals['acadyear'] = None;
+
+
             btree.update({str(len(btree) + 1): dict_cols_vals})
-            print(btree['5'])
 
         elif not value_list_bool and not column_name_bool: #third case input
             assignment_list = assignment_list.split(',')
@@ -464,77 +550,125 @@ if operation == 'insert':
 
             dict_cols_vals = dict(zip(col_name,value_list))
 
+            if 'studno' not in dict_cols_vals:
+                    dict_cols_vals['studno'] = None;
+            if 'cno' not in dict_cols_vals:
+                    dict_cols_vals['cno'] = None;
+            if 'semester' not in dict_cols_vals:
+                    dict_cols_vals['semester'] = None;
+            if 'acadyear' not in dict_cols_vals:
+                    dict_cols_vals['acadyear'] = None;
+
+
             btree.update({str(len(btree) + 1): dict_cols_vals})
-            print(btree['5'])
 
 
 print(list(btree))
 #print(btree["2012-29339"])
-
-select = 'studentname,birthday,studno'
 '''
-for j, k in zip(btree.keys(), btree.values()):
-    if select == '*':
-        print(j + " ", end="")
-        print(k['studentname'] + " ", end="")
-        print(k['birthday'] + " ", end="")
-        print(k['degree'] + " ", end="")
-        print(k['major'] + " ", end="")
-        print(k['unitsearned'] + " ", end="")
-        print("")
-    else:
-        if select == 'studno':
-            print(j)
-        else:
-            select_query = select.split(',')
-            for item in select_query:
-                if item == 'studno':
-                    print(j + " ", end="")
-                else:
-                    print(k[item] + " ", end="")
+elif operation == 'select':
+    select = 'studentname,birthday,studno'
+    
+    for j, k in zip(btree.keys(), btree.values()):
+        if select == '*':
+            print(j + " ", end="")
+            print(k['studentname'] + " ", end="")
+            print(k['birthday'] + " ", end="")
+            print(k['degree'] + " ", end="")
+            print(k['major'] + " ", end="")
+            print(k['unitsearned'] + " ", end="")
             print("")
-'''
-'''
-#for studenthistory
-for j, k in zip(btree.keys(), btree.values()):
-    if select == '*':
-        print(j + " ", end="")
-        print(k['description'] + " ", end="")
-        print(k['action'] + " ", end="")
-        print(k['datefiled'] + " ", end="")
-        print(k['dateresolved'] + " ", end="")
-        print("")
-    else:
-        if select == 'studno':
-            print(j)
         else:
-            select_query = select.split(',')
-            for item in select_query:
-                if item == 'studno':
-                    print(j + " ", end="")
-                else:
-                    print(k[item] + " ", end="")
+            if select == 'studno':
+                print(j)
+            else:
+                select_query = select.split(',')
+                for item in select_query:
+                    if item == 'studno':
+                        print(j + " ", end="")
+                    else:
+                        print(k[item] + " ", end="")
+                print("")
+    '''
+    
+'''
+    #for studenthistory
+    for j, k in zip(btree.keys(), btree.values()):
+        if select == '*':
+            print(j + " ", end="")
+            print(k['description'] + " ", end="")
+            print(k['action'] + " ", end="")
+            print(k['datefiled'] + " ", end="")
+            print(k['dateresolved'] + " ", end="")
             print("")
+        else:
+            if select == 'studno':
+                print(j)
+            else:
+                select_query = select.split(',')
+                for item in select_query:
+                    if item == 'studno':
+                        print(j + " ", end="")
+                    else:
+                        print(k[item] + " ", end="")
+                print("")
 
-#for course
-for j, k in zip(btree.keys(), btree.values()):
-    if select == '*':
-        print(j + " ", end="")
-        print(k['ctitle'] + " ", end="")
-        print(k['cdesc'] + " ", end="")
-        print(k['noofunits'] + " ", end="")
-        print(k['haslab'] + " ", end="")
-        print(k['semoffered'] + " ", end="")
-        print("")
-    else:
-        if select == 'cno':
-            print(j)
+    #for course
+    for j, k in zip(btree.keys(), btree.values()):
+        if select == '*':
+            print(j + " ", end="")
+            print(k['ctitle'] + " ", end="")
+            print(k['cdesc'] + " ", end="")
+            print(k['noofunits'] + " ", end="")
+            print(k['haslab'] + " ", end="")
+            print(k['semoffered'] + " ", end="")
+            print("")
+        else:
+            if select == 'cno':
+                print(j)
+            else:
+                select_query = select.split(',')
+                for item in select_query:
+                    if item == 'cno':
+                        print(j + " ", end="")
+                    else:
+                        print(k[item] + " ", end="")
+                print("")
+    '''
+'''
+#for courseoffering
+        select = "*"
+        for j, k in zip(btree.keys().split(','), btree.values()):
+            if select == '*':
+                print(j[0] + " ", end="")
+                print(j[1] + " ", end="")
+                print(j[2] + " ", end="")
+                print(j[3] + " ", end="")
+                #print(k['semester'] + " ", end="")
+                #print(k['acadyear'] + " ", end="")
+                #print(k['cno'] + " ", end="")
+                #print(k['section'] + " ", end="")
+                print(k['time'] + " ", end="")
+                print(k['maxstud'] + " ", end="")
+                print("")
+            else:
+                select_query = select.split(',')
+                for item in select_query:
+                    print(k[item] + " ", end="")
+                print("")
+'''
+'''
+#for studcourse
+    for j, k in zip(btree.keys(), btree.values()):
+        if select == '*':
+            print(k['studno'] + " ", end="")
+            print(k['cno'] + " ", end="")
+            print(k['semester'] + " ", end="")
+            print(k['acadyear'] + " ", end="")
+            print("")
         else:
             select_query = select.split(',')
             for item in select_query:
-                if item == 'cno':
-                    print(j + " ", end="")
-                else:
-                    print(k[item] + " ", end="")
+                print(k[item] + " ", end="")
             print("")
 '''
